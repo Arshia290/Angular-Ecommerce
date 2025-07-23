e7e700d62d23f48b9e41c30370db11ce9ab20873
Arshia Rashedzadeh <arshiarashedzadeh@gmail.com>
2025-07-23 13:17:32 -0400

    Added sample products to test list and created card component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { ProductsList } from './pages/products-list/products-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, ProductsList],
   template: `
     <app-header></app-header>
+    <app-products-list></app-products-list>
     <router-outlet></router-outlet>
   `,
   styles: [],
