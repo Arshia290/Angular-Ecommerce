2f2514ea72ddbf12e2ec0fe34d77b9327dbab7db
Arshia Rashedzadeh <arshiarashedzadeh@gmail.com>
2025-08-05 11:23:13 -0400

    Added cart service

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PrimaryButton } from '../primary-button/primary-button';
+import { Cart } from '../../services/cart';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +8,13 @@ import { PrimaryButton } from '../primary-button/primary-button';
   template: `
     <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between iems-center">
       <span class="text-xl">My Store</span>
-      <app-primary-button label="Cart" (btnClicked)="showButtonClicked"/>
+      <app-primary-button [label]="'Cart (' + cartService.cart().length + ')'"/>
     </div>
   `,
   styles: ``
 })
 export class Header {
-  showButtonClicked() {
-    console.log('Cart button clicked!');
-  }
+  
+  cartService = inject(Cart);
+
 }
--- src/app/components/primary-button/primary-button.ts
+++ src/app/components/primary-button/primary-button.ts
@@ -4,8 +4,11 @@ import { Component, input, output } from '@angular/core';
   selector: 'app-primary-button',
   imports: [],
   template: `
-    <button class="bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-90" (click)="btnClicked.emit()">
-      {{ label() }}
+    <button
+      class=" bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-90"
+      (click)="btnClicked.emit()"
+    >
+      <span class="text-md">{{ label() }}</span>
     </button>
   `,
   styles: ``
